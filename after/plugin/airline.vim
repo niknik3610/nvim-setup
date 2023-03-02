3b40a7ac2c3e4f1a1b928f4e7da4cd6b1f84f549
Nik H <n.harnish@lancaster.ac.uk>
2023-03-02 12:06:35 +0100

    init commit

---
+++ after/plugin/airline.vim
@@ -0,0 +1,10 @@
+let g:airline#extensions#tabline#enabled = 1
+let g:airline#extensions#tabline#formatter = 'unique_tail'
+
+let g:airline_powerline_fonts = 0
+
+if !exists('g:airline_symbols')
+  let g:airline_symbols = {}
+endif
+
+" Unicode symbols "
---
+++ lua/setup/set.lua
@@ -0,0 +1,36 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 50 
+
+vim.opt.colorcolumn = ""
+
+vim.g.mapleader = " "
+vim.g.netrw_banner = 0;
+
+--opens explorer
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+--closes tab
+vim.keymap.set("n", "<leader>bd", vim.cmd.bd)
+
+--forward and backward tab
+vim.keymap.set("n", "<leader>,", vim.cmd.bprevious)
+vim.keymap.set("n", "<leader>.", vim.cmd.bnext)
+
+--save
+vim.keymap.set("n", "<leader>s", vim.cmd.w)
+vim.api.nvim_set_keymap("n", "<leader>br", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true })
