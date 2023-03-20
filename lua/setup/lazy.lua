13ead9b9d8c322712971824bc5a5573e67026640
niknik3610 <niklas.harnish@hotmail.com>
2023-03-20 10:16:10 +0100

    replaced packer with lazy

--- lua/setup/lazy.lua
+++ lua/setup/lazy.lua
@@ -1,21 +1,6 @@
-require("setup")
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-
 vim.g.mapleader = " "
 
-require('lazy').setup({
+return require('lazy').setup({
     -- Packer can manage itself
     'joshdick/onedark.vim',
     'catppuccin/nvim',
@@ -58,5 +43,3 @@ require('lazy').setup({
         }
     },
 })
-
-require 'nvim-treesitter.install'.compilers = { "clang" }
