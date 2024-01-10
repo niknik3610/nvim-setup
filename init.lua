a210cf6d334a7d380d085f08a376afc2e45978f0
NikHarnish <niklas.harnish@hotmail.com>
2024-01-10 13:06:41 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -23,7 +23,7 @@ require('lazy').setup({
     'catppuccin/nvim',
 
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
         -- or                            , branch = '0.1.x',
         dependencies = { {'nvim-lua/plenary.nvim'} }
     },
